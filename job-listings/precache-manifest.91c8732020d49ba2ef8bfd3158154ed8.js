e27faecc92e98d39de9d3bd2a0aba683cf00f980
Caio Oliveira <caio.vsoliveira23@gmail.com>
2020-07-12 11:20:09 -0300

    Updates

---
+++ job-listings/precache-manifest.91c8732020d49ba2ef8bfd3158154ed8.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12b7a435159493f1ab312ece14d92b4c",
+    "url": "/frontend-mentor-challenges/job-listings/index.html"
+  },
+  {
+    "revision": "02079b23a1e024ccc814",
+    "url": "/frontend-mentor-challenges/job-listings/static/js/2.2db25147.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/frontend-mentor-challenges/job-listings/static/js/2.2db25147.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c927778e0fcb26c199ad",
+    "url": "/frontend-mentor-challenges/job-listings/static/js/main.fadf5073.chunk.js"
+  },
+  {
+    "revision": "d35c9b0c0accf4865a5e",
+    "url": "/frontend-mentor-challenges/job-listings/static/js/runtime-main.64777a1d.js"
+  },
+  {
+    "revision": "9f72c13bc10a76e4bcb8f7ab62582b9f",
+    "url": "/frontend-mentor-challenges/job-listings/static/media/account.9f72c13b.svg"
+  },
+  {
+    "revision": "6e2f7196c0644447b6887a48a265bdb1",
+    "url": "/frontend-mentor-challenges/job-listings/static/media/bg-header-desktop.6e2f7196.svg"
+  },
+  {
+    "revision": "6b5ff5a0e7f81b4c1247726c10c7aef5",
+    "url": "/frontend-mentor-challenges/job-listings/static/media/bg-header-mobile.6b5ff5a0.svg"
+  },
+  {
+    "revision": "39eaebdf1b82df9648392a9fb0915e9c",
+    "url": "/frontend-mentor-challenges/job-listings/static/media/eyecam-co.39eaebdf.svg"
+  },
+  {
+    "revision": "76faa79fba7225014706c0aac0b46472",
+    "url": "/frontend-mentor-challenges/job-listings/static/media/faceit.76faa79f.svg"
+  },
+  {
+    "revision": "ae67c678a143e6c6b913629f3d3b55e9",
+    "url": "/frontend-mentor-challenges/job-listings/static/media/icon-remove.ae67c678.svg"
+  },
+  {
+    "revision": "52b85a5018745489a50626498bc347ad",
+    "url": "/frontend-mentor-challenges/job-listings/static/media/insure.52b85a50.svg"
+  },
+  {
+    "revision": "6c50fd745d8d5c5e1dd47fa8c9029a6b",
+    "url": "/frontend-mentor-challenges/job-listings/static/media/loop-studios.6c50fd74.svg"
+  },
+  {
+    "revision": "fbe431a9e5d624447d698fee099ca843",
+    "url": "/frontend-mentor-challenges/job-listings/static/media/manage.fbe431a9.svg"
+  },
+  {
+    "revision": "e0ad9358d008658c14d4350d48627923",
+    "url": "/frontend-mentor-challenges/job-listings/static/media/myhome.e0ad9358.svg"
+  },
+  {
+    "revision": "3f34b49c96dcbb44450b43184cd6cd78",
+    "url": "/frontend-mentor-challenges/job-listings/static/media/photosnap.3f34b49c.svg"
+  },
+  {
+    "revision": "45f63772b29b7ccba3bc0c0c6e1d505d",
+    "url": "/frontend-mentor-challenges/job-listings/static/media/shortly.45f63772.svg"
+  },
+  {
+    "revision": "23b515f95978bcb24d704b1abecc8196",
+    "url": "/frontend-mentor-challenges/job-listings/static/media/the-air-filter-company.23b515f9.svg"
+  }
+]);
