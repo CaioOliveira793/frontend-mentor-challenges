ca38935030c19b9558ada64b67d058dfa80fe9fb
Caio Oliveira <caio.vsoliveira23@gmail.com>
2020-09-14 16:17:08 -0300

    feat(countries-viewer): load country details with external api

--- countries-viewer/src/modules/Country/index.tsx
+++ countries-viewer/src/modules/Country/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FaArrowLeft } from 'react-icons/fa';
 import { RouteComponentProps } from '@reach/router';
 
@@ -6,16 +6,53 @@ import Header from '../../components/Header';
 import Info from '../../components/Info';
 import Tag from '../../components/Tag';
 
+import countriesAPI from '../../services/countriesAPI';
+
 import { Container, NavigationContainer, StyledLink, CountryContentContainer,
 	FlagImage, CountryDetailsContainer, Name, InfoContainer, TagContainer } from './styles';
 
+interface CountryDetailData {
+	flag: string;
+	name: string;
+	nativeName: string;
+	population: number;
+	region: string;
+	subregion: string;
+	topLevelDomain: string[];
+	capital: string;
+	currencies: {
+		code: string;
+		name: string;
+	}[];
+	languages: {
+		name: string;
+	}[];
+	borders: {
+		name: string;
+	}[];
+}
+
+
 const Country: React.FC<RouteComponentProps> = ({ location }) => {
+	const [countryDetailsData, setCountryDetailsData] = useState<CountryDetailData>(null);
 	useEffect(() => {
 		const { pathname } = location;
 		const countryName = pathname.substring(pathname.lastIndexOf('/') + 1);
 
-		console.log(countryName);
-	}, [location]);
+		async function loadCountryDetails() {
+			const countryResponse = await countriesAPI.get<CountryDetailData[]>(`/name/${countryName}?fullText=true`);
+			const countryData = countryResponse.data[0];
+
+			const borderCountryResponse = await countriesAPI.get(`/alpha?codes=${countryData.borders.join(';')}`);
+			const borderCountryData = borderCountryResponse.data;
+
+			setCountryDetailsData({
+				...countryData,
+				borders: borderCountryData
+			});
+		}
+		loadCountryDetails();
+	}, [location, setCountryDetailsData]);
 
 
 	return (
@@ -28,29 +65,30 @@ const Country: React.FC<RouteComponentProps> = ({ location }) => {
 				</StyledLink>
 			</NavigationContainer>
 			<CountryContentContainer>
-				<FlagImage src="https://restcountries.eu/data/bra.svg" />
-				<CountryDetailsContainer>
-					<Name>Brazil</Name>
-					<InfoContainer>
-						<Info infoKey="Native Name" infoValue="Brasil" />
-						<Info infoKey="Population" infoValue="1100000000" />
-						<Info infoKey="Region" infoValue="Americas" />
-						<Info infoKey="Sub Region" infoValue="South America" />
-						<Info infoKey="Capital" infoValue="Brasília" />
-						<Info infoKey="Top Level Domain" infoValue=".br" />
-						<Info infoKey="Currencies" infoValue="Real" />
-						<Info infoKey="Languages" infoValue="Português" />
-					</InfoContainer>
-					<TagContainer>
-						Border Countries:
-						<Tag>France</Tag>
-						<Tag>France</Tag>
-						<Tag>France</Tag>
-						<Tag>Germany</Tag>
-						<Tag>Germany</Tag>
-						<Tag>Germany</Tag>
-					</TagContainer>
-				</CountryDetailsContainer>
+				{countryDetailsData &&
+					<>
+						<FlagImage src={countryDetailsData.flag} />
+						<CountryDetailsContainer>
+							<Name>{countryDetailsData.name}</Name>
+							<InfoContainer>
+								<Info infoKey="Native Name" infoValue={countryDetailsData.nativeName} />
+								<Info infoKey="Population" infoValue={countryDetailsData.population} />
+								<Info infoKey="Region" infoValue={countryDetailsData.region} />
+								<Info infoKey="Sub Region" infoValue={countryDetailsData.subregion} />
+								<Info infoKey="Capital" infoValue={countryDetailsData.capital} />
+								<Info infoKey="Top Level Domain" infoValue={countryDetailsData.topLevelDomain[0]} />
+								<Info infoKey="Currencies" infoValue={countryDetailsData.currencies[0].name} />
+								<Info infoKey="Languages" infoValue={countryDetailsData.languages[0].name} />
+							</InfoContainer>
+							<TagContainer>
+								Border Countries:
+								{countryDetailsData.borders.map(({ name }) => (
+									<Tag>{name}</Tag>
+								))}
+							</TagContainer>
+						</CountryDetailsContainer>
+					</>
+				}
 			</CountryContentContainer>
 		</Container>
 	);
