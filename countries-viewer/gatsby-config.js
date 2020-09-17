0f2f20e3c9a0b992f1d46747a00b6396d01cba31
Caio Oliveira <caio.vsoliveira23@gmail.com>
2020-09-17 15:53:17 -0300

    chore(countries-viewer): add gatsby plugins

--- countries-viewer/gatsby-config.js
+++ countries-viewer/gatsby-config.js
@@ -6,18 +6,48 @@
 
 module.exports = {
 	/* Your site config here */
+	siteMetadata: {
+		title: 'Countries Viewer Solution',
+		siteUrl: 'https://caiooliveira793.github.io/frontend-mentor-challenges/countries-viewer/'
+	},
+
 	plugins: [
+		`gatsby-plugin-react-helmet`,
+		`gatsby-plugin-sitemap`,
+		`gatsby-transformer-sharp`,
+		`gatsby-plugin-sharp`,
 		'gatsby-plugin-styled-components',
 		{
       resolve: `gatsby-plugin-typescript`,
       options: {},
     },
 		{
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Countries Viewer Solution`,
+        short_name: `Countries Viewer`,
+        start_url: `/`,
+				icon: `static/favicon.ico`, // 512x512
+        background_color: `#202C37`,
+        theme_color: `#2B3945`,
+        display: `standalone`,
+      },
+    },
+		// {
+    //   resolve: `gatsby-plugin-google-analytics`,
+    //   options: {},
+    // },
+		{
 			resolve: `gatsby-plugin-create-client-paths`,
 			options: {
 				prefixes: [`/countries/*`]
 			}
-		}
+		},
+		{
+			resolve: `gatsby-plugin-offline`,
+			options: {}
+		},
 	],
+
 	pathPrefix: `/frontend-mentor-challenges/countries-viewer`,
 }
